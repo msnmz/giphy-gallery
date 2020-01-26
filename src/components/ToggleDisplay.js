f8ae40bf968b64110a1e812a46b04fd96ab338d9
msnmz <snmez.muharrem@gmail.com>
2020-01-26 01:18:34 +0100

    Creates toggle component

---
+++ src/components/ToggleDisplay.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import './ToggleDisplay.css';
+
+export default function ToggleDisplay({ onToggle }) {
+  const [oneColumn, setOneColumn] = useState(true);
+
+  function handleToggle(toOneColumnView) {
+    setOneColumn(toOneColumnView);
+    onToggle(toOneColumnView);
+  }
+
+  return (
+    <div className='toggle-container'>
+      <button onClick={() => handleToggle(true)} className={'toggle-button ' + oneColumn ? 'active' : 'passive'}>1-Column Display</button>
+      <div className='divider'></div>
+      <button onClick={() => handleToggle(false)} className={'toggle-button ' + oneColumn ? 'passive' : 'active'}>3-Column Display</button>
+    </div>
+  )
+} 
